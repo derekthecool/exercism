56e948a0e4818080ef24a16ccaffa676a649c0d1
Derek Lomax <derekthecool@gmail.com>
2025-09-11 09:20:02 -0600

    feat: get secret-handshake working in powershell

--- powershell/secret-handshake/SecretHandshake.ps1
+++ powershell/secret-handshake/SecretHandshake.ps1
@@ -1,38 +1,15 @@
-Function Invoke-SecretHandshake() {
-    <#
-    .SYNOPSIS
-    Convert a number between 1 and 31 to a sequence of actions in the secret handshake.
-
-    .DESCRIPTION
-    The sequence of actions is chosen by looking at the rightmost five digits of the number once it's been converted to binary.
-    Start at the right-most digit and move left.
-
-    The actions for each number place are:
-    00001 = wink
-    00010 = double blink
-    00100 = close your eyes
-    01000 = jump
-    10000 = Reverse the order of the operations in the secret handshake.
-     #>
+function Invoke-SecretHandshake() {
     param(
         [int]$Number
     )
-    $actions = [ordered]@{
-        <#1 = 'wink'
-        2 = 'double blink'
-        4 = 'close your eyes'
-        #>
-        8 = 'jump'
-        4 = 'close your eyes'
-        2 = 'double blink'
-        1 = 'wink'
-    }
-    $result = $actions.Keys | ?{$Number -band $_} | %{$actions[$_]}
-    <#
-    if(($Number -band 16))
-    {
-        [array]::Reverse($result)
-    }
-    #>
-    $result
+
+$actions = @{
+    '1' = 'wink'
+    '2' = 'double blink'
+    '4' = 'close your eyes'
+    '8' = 'jump'
+}
+
+# Get matching actions
+$actions.Keys | Sort-Object -Descending:$($Number -band 16) | Where-Object { $_ -band $Number } | ForEach-Object { $actions[$_] }
 }
