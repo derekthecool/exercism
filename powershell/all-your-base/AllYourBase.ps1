6c3f040cc19f3899ec63ef3105a5d68588cda2be
Derek Lomax <derekthecool@gmail.com>
2025-09-15 10:16:18 -0600

    feat: some progress on all your base powershell

--- powershell/all-your-base/AllYourBase.ps1
+++ powershell/all-your-base/AllYourBase.ps1
@@ -1,4 +1,4 @@
-Function Invoke-Rebase() {
+function Invoke-Rebase() {
     <#
     .SYNOPSIS
     Convert a number, represented as a sequence of digits in one base, to any other base.
@@ -27,16 +27,34 @@ Function Invoke-Rebase() {
         [int]$OutputBase
     )
 
-    Write-Host "Digits: $Digits, InputBase: $InputBase, OutputBase: $OutputBase"
+  if($InputBase -le 1)
+  {
+      throw 'input base must be >= 2'
+  }
+  if($OutputBase -le 1)
+  {
+      throw 'output base must be >= 2'
+  }
+  foreach($number in $Digits)
+  {
+     if($number -ge $InputBase -or $number -lt 0)
+     {
+       throw 'all digits must satisfy 0 <= digit < input base'
+     }
+  }
+  
+  
+
+    Write-Verbose "Digits: $Digits, InputBase: $InputBase, OutputBase: $OutputBase"
 
     $recoveredInput = 0
     for($DigitIndex = $Digits.Length - 1; $DigitIndex -ge 0; $DigitIndex--){
         $currentNumberAsBase10 = $Digits[$DigitIndex]
         $currentInputBaseSegment = $currentNumberAsBase10 * [Math]::Pow($InputBase,$DigitIndex)
         $recoveredInput += $currentInputBaseSegment
-        # Write-Host "currentNumberAsBase10 = $currentNumberAsBase10"
-        # Write-Host "currentInputBaseSegment = $currentInputBaseSegment"
-        # Write-Host "recoveredInput = $recoveredInput"
+        # Write-Verbose "currentNumberAsBase10 = $currentNumberAsBase10"
+        # Write-Verbose "currentInputBaseSegment = $currentInputBaseSegment"
+        # Write-Verbose "recoveredInput = $recoveredInput"
 
         # PWSH 7 ternary operator to determine the equationEnding text
         $equationEnding = $DigitIndex -ne 0 ? ' + ' : ''
@@ -44,11 +62,21 @@ Function Invoke-Rebase() {
         $equationString += "($currentNumberAsBase10 x $InputBase ^ $DigitIndex)$equationEnding"
     }
 
-    Write-Host "$equationString = $recoveredInput"
+    Write-Verbose "$equationString = $recoveredInput"
 
-    $recoveredInputAsString = "$recoveredInput"
-    for($OutputDigitIndex = 0; $OutputDigitIndex -lt $recoveredInputAsString.Length; $OutputDigitIndex++){
-        $currentNumberForOutputBaseAsBase10 = [int]$recoveredInputAsString[$OutputDigitIndex]
-        Write-Host "currentNumberForOutputBaseAsBase10 = $currentNumberForOutputBaseAsBase10"
-    }
+    # $recoveredInputAsString = "$recoveredInput"
+    # for($OutputDigitIndex = 0; $OutputDigitIndex -lt $recoveredInputAsString.Length; $OutputDigitIndex++){
+    #     $currentNumberForOutputBaseAsBase10 = [int]$recoveredInputAsString[$OutputDigitIndex]
+    #     Write-Verbose "currentNumberForOutputBaseAsBase10 = $currentNumberForOutputBaseAsBase10"
+    # }
+  #
+  $OutputResult = @()
+   do {
+    $Calculation = [Math]::DivRem($recoveredInput, $OutputBase)
+    $recoveredInput = $Calculation.Item1
+    $OutputResult += $Calculation.Item2
+    Write-Verbose "[Math]::DivRem result where first item is dividend and second item is remainder: $Calculation"
+   } while ($Calculation.Item1 -gt 0)
+
+  $OutputResult
 }
